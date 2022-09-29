25f30b89b204797ddf55147964d911a4603eafdc
escontrerass <edgarsamuel.contreras@unet.edu.ve>
2022-09-29 18:21:09 -0400

    Responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,22 @@
+import { GitHub } from '@mui/icons-material'
+import { AppBar, CardMedia, IconButton, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import react_logo from '../assets/react_logo.png'
 
 export const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <AppBar position='static' color='warning' sx={{ height: '8vh', justifyContent: 'center' }}>
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        <IconButton>
+          <img src={react_logo} alt='React logo' width='50px' />
+        </IconButton>
+        <Typography>
+          Power By <IconButton>PokeApi</IconButton>
+        </Typography>
+        <IconButton href=''>
+          <GitHub />{' '}
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   )
 }
