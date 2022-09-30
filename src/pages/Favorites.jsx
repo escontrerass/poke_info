4881a6032bd5f46924aa1df61b43801410385478
escontrerass <edgarsamuel.contreras@unet.edu.ve>
2022-09-30 19:38:33 -0400

    add button back favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Main } from '../containers/Main'
-import { CircularProgress, Box, Grid } from '@mui/material'
+import { CircularProgress, Box, Grid, Button } from '@mui/material'
 import { PokeCard } from '../components/PokeCard'
 import { useLocalStorage } from '../hooks/useLocalStorage'
 import { ListFavorites } from '../utils/fetchFavorite'
 import { NotFavorites } from '../components/NotFavorites'
+import { Link } from 'react-router-dom'
 
 export const Favorites = () => {
   const [listFavorite] = useLocalStorage()
@@ -19,20 +20,29 @@ export const Favorites = () => {
           <CircularProgress color='warning' size='325px' />
         </Box>
       ) : (
-        <Grid
-          container
-          py='32px'
-          px={{ md: '80px' }}
-          rowSpacing={4}
-          columnGap={{ sm: 2, md: 4, lg: 8 }}
-          sx={{ justifyContent: 'center', alignItems: 'center' }}
-        >
-          {listData.map((data, index) => (
-            <Grid item xs={11} sm={5} md={3.6} lg={2.5} key={index}>
-              <PokeCard {...data} />
-            </Grid>
-          ))}
-        </Grid>
+        <>
+          <Grid
+            container
+            py='32px'
+            px={{ md: '80px' }}
+            rowSpacing={4}
+            columnGap={{ sm: 2, md: 4, lg: 8 }}
+            sx={{ justifyContent: 'center', alignItems: 'center' }}
+          >
+            {listData.map((data, index) => (
+              <Grid item xs={11} sm={5} md={3.6} lg={2.5} key={index}>
+                <PokeCard {...data} />
+              </Grid>
+            ))}
+          </Grid>
+          <Link to='/'>
+            <Box sx={{ textAlign: 'center' }}>
+              <Button color='primary' variant='contained' size='large'>
+                Volver a inicio.
+              </Button>
+            </Box>
+          </Link>
+        </>
       )}
     </Main>
   )
